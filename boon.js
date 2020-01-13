23895f97652b97dfab9e302ab72ac461967aa13d
jir <ichhabadhs@posteo.de>
2020-01-13 20:36:05 +0100

    js

---
+++ boon.js
@@ -0,0 +1,207 @@
+"use strict";
+class Boon {
+    constructor(elements) {
+        if (!Array.isArray(elements))
+            throw new Error("no array");
+        this.elements = Array.from(elements);
+        for (let i = 0; i < this.elements.length; i++) {
+            this[i] = this.elements[i];
+        }
+    }
+    each(func) {
+        this.elements.forEach(element => func(element));
+        return this;
+    }
+    get length() {
+        return this.elements.length;
+    }
+    get all() {
+        return this.elements;
+    }
+    copy() {
+        return new Boon(this.elements.slice());
+    }
+
+    //+++CLASS+++
+    addClass(c) {
+        return this.each(element => element.classList.add(c));
+    }
+    removeClass(c) {
+        return this.each(element => element.classList.remove(c));
+    }
+    toggleClass(c) {
+        return this.each(element => element.classList.toggle(c));
+    }
+    //---CLASS---
+    parent() {
+        return new Boon([...new Set(this.elements.map(element => element.parentElement).filter(e => e))]);
+    }
+    //+++EVENT+++
+    on(type, listener) {
+        return this.each(element => element.addEventListener(type, listener));
+    }
+    click(listener) {
+        return this.on("click", listener);
+    }
+    hover(enterListener, leaveListener) {
+        if (enterListener) {
+            this.on("mouseenter", enterListener);
+        }
+        if (leaveListener) {
+            this.on("mouseleave", leaveListener);
+        }
+        return this;
+    }
+    //---EVENT---
+    show() {
+        return this.each(element => element.style.display = null);
+    }
+    hide() {
+        return this.each(element => element.style.display = "none");
+    }
+    find(selector) {
+        return new Boon([...new Set(Array.from(this.elements.map(element => element.querySelectorAll(selector))).map(nodeList => Array.from(nodeList)).flat())]);
+    }
+    attr(attribute, value) {
+        if (value === undefined) {
+            let element = this.getElement();
+            if (element) {
+                return element.getAttribute(attribute);
+            }
+        } else {
+            return this.each(element => value == null ? element.removeAttribute(attribute) : element.setAttribute(attribute, value));
+        }
+    }
+    prop(property, value) {
+        if (value === undefined) {
+            let element = this.getElement();
+            if (element) {
+                return element[property];
+            }
+        } else {
+            return this.each(element => element[property] = value);
+        }
+    }
+    val(value) {
+        return this.prop("value", value);
+    }
+    filter(predicate) {
+        return new Boon(this.elements.filter(predicate));
+    }
+}
+const boon = function (value, argument) {
+    if (!value) return;
+    let valueType = typeof value;
+    if (value instanceof Boon) {
+        return new Boon(value.elements);
+    } else
+        if (valueType == "function") {
+            if (document.readyState == "complete") {
+                value(argument);
+            } else {
+                document.addEventListener("DOMContentLoaded", value);
+            }
+        } else if ((valueType == "object" && (value.nodeType == Node.ELEMENT_NODE || value.nodeType == Node.DOCUMENT_NODE))//
+            || valueType == "string") {
+            return valueType == "object" ? new Boon([value]) : new Boon(Array.from((argument || document).querySelectorAll(value)));
+        } else {
+            return new Boon([]);
+        }
+}
+
+window.boon = boon;
+if (!window.$) {
+    window.$ = window.boon;
+}
+
+boon(function () {
+    document.write("d");
+})
+
+boon.ajax = function (settings, success, error) {
+    if (typeof settings != "object") { throw new Error("wrong settings"); }
+    let contentType;
+    let accept;
+    let dataType = (settings.dataType || "").toLowerCase();
+    if (!dataType || dataType == "text" || dataType == "txt") {
+    } else if (dataType == "json") {
+        accept = contentType = "application/json";
+    } else if (dataType == "xml") {
+        accept = contentType = "application/xml";
+    } else if (dataType == "css") {
+        accept = contentType = "text/css";
+    } else if (dataType == "html") {
+        accept = contentType = "application/xhtml+xml";
+    }
+    if (accept) {
+        accept += ",*/*;q=0.1"
+    }
+
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4) {
+            let result = settings.responseConverter ? settings.responseConverter(this.response) : this.response;
+            if (this.status >= 200 && this.status < 300) {
+                if (success) {
+                    success(result);
+                } else if (settings.success) {
+                    settings.success(result);
+                }
+            } else {
+                if (error) {
+                    error(result);
+                } else if (settings.fail) {
+                    settings.fail(result);
+                } else if (settings.error) {
+                    settings.error(result);
+                }
+            }
+
+        }
+    };
+    xhttp.onloadend = settings.post;
+    xhttp.timeout = typeof settings.timeout == "number" ? settings.timeout : 0;
+    let url = settings.url || "";
+    if (typeof settings.params == "object") {
+        url += (window.location.search ? "&" : "?")
+            + Object.entries(settings.params || {}).map(e => {
+                let val = e[1];
+                if (val == undefined || val == null) {
+                    return;
+                }
+                if (typeof val == "object") {
+                    val = JSON.stringify(val);
+                }
+                return e[0] + "=" + encodeURIComponent(val);
+            }).join("&");
+    }
+    xhttp.open(settings.method || "get", url, true);
+    if (settings.pre) { settings.pre(); }
+    if (accept) {
+        xhttp.setRequestHeader("Accept", accept);
+    }
+    if (contentType) {
+        xhttp.setRequestHeader("Content-Type", contentType);
+    }
+    if (settings.mime) {
+        xhttp.setRequestHeader("Accept", mime);
+        xhttp.setRequestHeader("Content-Type", mime);
+    }
+    for (let [key, value] of Object.entries(settings.headers || {})) {
+        xhttp.setRequestHeader(key, value);
+    }
+    return xhttp.send(typeof settings.data == "object" ? JSON.stringify(settings.data) : (settings.data || "").toString());
+}
+boon.get = (settings, success, error) => {
+    settings.method = "get";
+    boon.ajax(settings, success, error);
+};
+boon.post = (settings, success, error) => {
+    settings.method = "post";
+    boon.ajax(settings, success, error);
+};
+boon.json = (settings, success, error) => {
+    settings.responseConverter = JSON.parse;
+    settings.dataType = "json";
+    boon.get(settings, success, error);
+};
